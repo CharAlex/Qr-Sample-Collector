a4f0cd246dae73ad8e832921069371b3a9e289bd
Alex Charantzidis <alexandros8a8@gmail.com>
2020-05-10 10:23:55 +0300

    Added Geolocation

--- app/src/main/java/com/alexchar_dev/samplecollector/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/alexchar_dev/samplecollector/presentation/ui/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.alexchar_dev.samplecollector.presentation.ui
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.alexchar_dev.samplecollector.R
 import com.alexchar_dev.samplecollector.data.models.LoginRequest
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private val camPermissionCode = 1001
     private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,12 +24,34 @@ class MainActivity : AppCompatActivity() {
             viewModel.login(LoginRequest("test@test.test", "testcandpass")).observe(this, androidx.lifecycle.Observer {
                 accessTkn.text = it.access
                 Toast.makeText(applicationContext, "log in success", Toast.LENGTH_SHORT).show()
-                startQrCodeActivity()
+                askCamPermission()
             })
         }
 
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == camPermissionCode && grantResults.isNotEmpty()) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startQrCodeActivity()
+            } else {
+                Toast.makeText(applicationContext, "Please grant camera permission", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun askCamPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            camPermissionCode
+        )
+    }
     private fun startQrCodeActivity() {
         startActivity(Intent(this, QrScanActivity::class.java))
     }
--- app/src/main/java/com/alexchar_dev/samplecollector/presentation/ui/QrScanActivity.kt
+++ app/src/main/java/com/alexchar_dev/samplecollector/presentation/ui/QrScanActivity.kt
@@ -1,47 +1,149 @@
 package com.alexchar_dev.samplecollector.presentation.ui
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.os.Looper
+import android.provider.Settings
 import android.view.SurfaceHolder
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.util.isNotEmpty
 import com.alexchar_dev.samplecollector.R
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.android.gms.vision.CameraSource
 import com.google.android.gms.vision.Detector
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
 import kotlinx.android.synthetic.main.activity_qr_scan.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.lang.Exception
+import kotlin.math.round
+
 
 class QrScanActivity : AppCompatActivity() {
 
     private val camPermissionCode = 1001
+    private val locPermissionCode = 1002
     private lateinit var cameraSource: CameraSource //Manages the camera in conjunction with an underlying Detector
     private lateinit var detector: BarcodeDetector
+    var mFusedLocationClient: FusedLocationProviderClient? = null
     private val viewModel: QrScanViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr_scan)
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             askCamPermission()
-        } else { //TODO move permission check before accessing this activity
+        } else {
             setUpControls()
         }
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            askLocationPermission()
+        } else {
+            getLocation()
+        }
+
         scanBtn.setOnClickListener {
-            viewModel.sendQrGeolocation(serialCode.text.toString(), 40.22, 12.00).observe(this, androidx.lifecycle.Observer {
+            val (x,y) = getFormattedLoc()
+            viewModel.sendQrGeolocation(serialCode.text.toString(), x, y).observe(this, androidx.lifecycle.Observer {
                 Toast.makeText(applicationContext, "response code ${it.code()}", Toast.LENGTH_SHORT).show()
             })
         }
+    }
+
+    private fun getFormattedLoc(): Pair<Double,Double> {
+        return Pair(latLoc.text.toString().toDouble().round(), longLoc.text.toString().toDouble().round())
+    }
+
+    private fun Double.round(decimals: Int = 6): Double {
+        var multiplier = 1.0
+        repeat(decimals) { multiplier *= 10 }
+        return round(this * multiplier) / multiplier
+    }
+
+    private fun getLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient?.lastLocation?.addOnCompleteListener { task ->
+                    val location: Location? = task.getResult()
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        latLoc.text = location.latitude.toString()
+                        longLoc.text = location.longitude.toString()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            askLocationPermission()
+        }
+    }
+
+    private fun requestNewLocationData() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        (mFusedLocationClient as FusedLocationProviderClient).requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            latLoc.text = mLastLocation.latitude.toString()
+            longLoc.text = mLastLocation.longitude.toString()
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun askLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            locPermissionCode
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
     }
 
     private fun setUpControls() {
@@ -70,6 +172,7 @@ class QrScanActivity : AppCompatActivity() {
         if(requestCode == camPermissionCode && grantResults.isNotEmpty()) {
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 setUpControls()
+                getLocation()
             } else {
                 Toast.makeText(applicationContext, "Please grant camera permission", Toast.LENGTH_SHORT).show()
             }
